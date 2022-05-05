b0707e2ec46ff48c671dabc28a3333f3796c0124
uo271620 <adolfo.r.s@hotmail.es>
2022-05-05 22:47:28 +0200

    Arreglo Traduccion

--- js/comentarios.js
+++ js/comentarios.js
@@ -1,21 +1,37 @@
 misComentarios = new Object;
+misComentarios.count = 2;
 
 misComentarios.publicar = function(){
     var asunto = $("#asunto").val();
     var comentario = $("#contenido").val();
+    if (asunto!="" &&comentario!=""){
+        $("#comentarios").append("<section><h2>"+asunto+"</h2><p id='comentario-0"+ 
+        misComentarios.count +"'>"+comentario+"</p>"+
+        "<label for='source-0"+misComentarios.count+"'> Traducir de</label>"+
+        "<select id='source-0"+misComentarios.count+"'>"+
+        "<option value='en'>en</option><option value='es' selected>es</option><option value='fr'>fr</option>"+
+        "</select>" +
+        "<label for='target-0"+misComentarios.count+"'> Traducir de</label>"+
+        "<select id='target-0"+misComentarios.count+"'>"+
+        "<option value='en' selected>en</option><option value='es'>es</option><option value='fr'>fr</option>"+
+        "</select>" +
+        " <button id='traductor-0"+misComentarios.count+"' onclick='traductor.traduce(this.id)'>Traducir</button>"
+        +"</section>");
 
-    $("#comentarios").append("<section><h2>"+asunto+"</h2><p>"+comentario+"</p></section>");
-    $("#asunto").text("");
-    $("#contenido").text("");
+        misComentarios.count = misComentarios.count + 1;
+
+        $("#asunto").val('');
+        $("#contenido").val('');
+    }
 }
-
-$(document).ready(function(){
-	$("#enviar").click(function(){
-        misComentarios.publicar();
+misComentarios.setButons = function (){
+    $(document).ready(function(){
+        $("#enviar").click(function(){
+            misComentarios.publicar();
+        });
     });
-});
-
-
+}
+misComentarios.setButons();
 
 
 
--- js/traductor.js
+++ js/traductor.js
@@ -1,12 +1,12 @@
 var traductor = new Object();
 
 traductor.traduce = function (buttonid) {
+    var id = buttonid.split('-')[1];
     var commentId = "comentario-"+buttonid.split('-')[1];
 
-    
     this.settings.data.q = $("#"+commentId).text();
-    this.settings.data.source=$("#source option:selected").val();
-    this.settings.data.target= $("#target option:selected").val();
+    this.settings.data.source=$("#source-"+id+" option:selected").val();
+    this.settings.data.target= $("#target-"+id+" option:selected").val();
 
     $.ajax(this.settings).done(function (response) {
         
@@ -28,7 +28,7 @@ traductor.configuraAPI = function() {
 			"method": "POST",
 			"headers": {
 				"content-type": "application/x-www-form-urlencoded",
-				"x-rapidapi-key": "62ec1162b5msh330582a3b663638p163899jsn147ac89e094c",
+				"x-rapidapi-key": "ece741474fmsh2da6a88d7f0d0a5p172390jsn3eb49e9a90e7",
 				"x-rapidapi-host": "google-translate1.p.rapidapi.com"
 			},
 			"data": {
